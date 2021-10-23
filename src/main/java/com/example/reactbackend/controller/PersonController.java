a387cc9bf71c98220c4cd0d82b0798f7774aef3f
lukasze <wyspianski.lukasz@gmail.com>
2021-10-23 19:34:45 +0200

    Glue the Spring layers - @Autowired (constructor)

--- src/main/java/com/example/reactbackend/controller/PersonController.java
+++ src/main/java/com/example/reactbackend/controller/PersonController.java
@@ -1,7 +1,14 @@
 package com.example.reactbackend.controller;
 
+import com.example.reactbackend.service.PersonService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
+
+    private PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
 }
--- src/main/java/com/example/reactbackend/service/PersonService.java
+++ src/main/java/com/example/reactbackend/service/PersonService.java
@@ -1,7 +1,14 @@
 package com.example.reactbackend.service;
 
+import com.example.reactbackend.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
+
+    private PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 }
