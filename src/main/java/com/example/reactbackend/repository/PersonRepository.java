bba98738c723b16eda2a46757e2a209080118924
lukasze <wyspianski.lukasz@gmail.com>
2021-10-23 19:33:13 +0200

    @Entity & Spring superpowers added to regular Java classes

--- src/main/java/com/example/reactbackend/repository/PersonRepository.java
+++ src/main/java/com/example/reactbackend/repository/PersonRepository.java
@@ -1,4 +1,7 @@
 package com.example.reactbackend.repository;
 
-public interface PersonRepository {
+import com.example.reactbackend.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
--- src/main/java/com/example/reactbackend/service/PersonService.java
+++ src/main/java/com/example/reactbackend/service/PersonService.java
@@ -1,4 +1,7 @@
 package com.example.reactbackend.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonService {
 }
