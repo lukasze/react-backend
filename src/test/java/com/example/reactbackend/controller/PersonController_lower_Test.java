1ea7cb1d328a3792e5360f381cc6d466d3029311
uki <wyspianski.lukasz@gmail.com>
2021-10-26 12:18:01 +0200

    Test for "upper"

--- src/test/java/com/example/reactbackend/controller/PersonController_lower_Test.java
+++ src/test/java/com/example/reactbackend/controller/PersonController_lower_Test.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("lower")
-class PersonControllerTest {
+class PersonController_lower_Test {
 
     @Autowired
     private MockMvc mockMvc;
