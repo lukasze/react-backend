a830811d6a6d9ff9b4429598ed1722d1b48fa862
uki <wyspianski.lukasz@gmail.com>
2021-10-26 13:29:56 +0200

    Test part 2

--- src/test/java/com/example/reactbackend/service/PersonServiceTest.java
+++ src/test/java/com/example/reactbackend/service/PersonServiceTest.java
@@ -8,6 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
@@ -25,7 +27,38 @@ class PersonServiceTest {
     private Person person = new Person(1l, firstName, lastName);
 
     @Test
-    void find() {
+    void find_upper() {
+        when(personRepository.findById(1L)).thenReturn(Optional.of(person));
 
+        Person person = personService.find(1L);
+
+        assertAll(
+                () -> assertEquals(firstName.toUpperCase(), person.getFirstName()),
+                () -> assertEquals(lastName.toUpperCase(), person.getLastName())
+        );
+    }
+
+    @Test
+    void find_lower() {
+        when(personRepository.findById(2L)).thenReturn(Optional.of(person));
+
+        Person person = personService.find(2L);
+
+        assertAll(
+                () -> assertEquals(firstName.toLowerCase(), person.getFirstName()),
+                () -> assertEquals(lastName.toLowerCase(), person.getLastName())
+        );
+    }
+
+    @Test
+    void find_unchanged() {
+        when(personRepository.findById(3L)).thenReturn(Optional.of(person));
+
+        Person person = personService.find(3L);
+
+        assertAll(
+                () -> assertEquals(firstName, person.getFirstName()),
+                () -> assertEquals(lastName, person.getLastName())
+        );
     }
 }
