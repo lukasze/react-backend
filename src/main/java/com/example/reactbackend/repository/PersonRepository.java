659ccd8b647f1c2cb5b94fff715d4a84e320cb57
lukasze <wyspianski.lukasz@gmail.com>
2021-11-08 11:18:47 +0100

    Tests with fail()

--- src/main/java/com/example/reactbackend/repository/PersonRepository.java
+++ src/main/java/com/example/reactbackend/repository/PersonRepository.java
@@ -3,5 +3,7 @@ package com.example.reactbackend.repository;
 import com.example.reactbackend.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
---
+++ src/test/java/com/example/reactbackend/repository/PersonRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.example.reactbackend.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonRepositoryTest {
+
+    @Test
+    void shouldFindYuval_usingCustomRepoMethod() {
+        fail();
+    }
+
+    @Test
+    void shouldFindYuval_usingQuery(){
+        fail();
+    }
+
+    @Test
+    void shouldFindYuval_usingNativeQuery(){
+        fail();
+    }
+
+    @Test
+    void shouldFindYuval_usingNamedQuery(){
+        fail();
+    }
+
+    @Test
+    void shouldFindYuval_usingNamedNativeQuery(){
+        fail();
+    }
+
+    @Test
+    void shouldFindYuval_usingCustomImplementation(){
+        fail();
+    }
+
+
+
+
+}
